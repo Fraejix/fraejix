8a9b4b7a2650968ba9120ea154c6a25c2089ad9c
Fraejix <BlackBeltJosh@gmail.com>
2024-04-13 08:53:23 -0500

    codevolution next.js 14 tutorial 11-20

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function ProductList() {
+    const productId = 100
     return (
     <>
+        <Link href="/">Home</Link>
         <h1>Product List</h1>
-        <h2><a href="products/1">Product 1</a></h2>
-        <h2><a href="products/2">Product 2</a></h2>
-        <h2><a href="products/3">Product 3</a></h2>
+        <h2><Link href="products/1">Product 1</Link></h2>
+        <h2><Link href="products/2">Product 2</Link></h2>
+        <h2><Link href="products/3">Product 3</Link></h2>
+        <h2><Link href={`products/${productId}`}>Product {productId}</Link></h2>
     </>
     )
   }
